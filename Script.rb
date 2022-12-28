ebb4407b8362c267daa33d4275a2f4bb6c2d3124
FL <fl50000@hotmail.com>
2022-12-28 08:06:14 -0300

    General refactor/adjusts

--- Script.rb
+++ Script.rb
@@ -2,30 +2,34 @@
 # * Time of Day Introduction - by FL (Credits will be apreciated)
 #===============================================================================
 #
-# This script is for Pokémon Essentials. It show Time of Day message and 
+# This script is for Pokémon Essentials. It show time of day message and 
 # image (i.e. "Day", "Night"). 
 #
-#===============================================================================
+#== INSTALLATION ===============================================================
 #
-# To this script works, put it above main. Place introductionday.png and 
-# introductionnight.png images on Graphics/Pictures . If you don't put, the
-# message will be centralized on black background.
+# Put it above main OR convert into a plugin.
 #
-# The time is displayed when player came outside and time was changed and when
+#== NOTES ======================================================================
+#
+# The intro is displayed when player came outside and time was changed and when
 # time change and player already is outside.
+#
+# You can create a "Time Intro" folder at Graphics/Pictures and put the day.png
+# and night.png pictures to show as intro images. If you don't put, the message
+# will be centralized on a black background.
 # 
 #===============================================================================
 
 if !PluginManager.installed?("Time of Day Introduction")
   PluginManager.register({                                                 
     :name    => "Time of Day Introduction",                                        
-    :version => "1.0.1",                                                     
-    :link    => "https://www.pokecommunity.com/showthread.php?p=10369544",             
+    :version => "1.0.2",                                                     
+    :link    => "https://www.pokecommunity.com/showthread.php?t=453548",             
     :credits => "FL"
   })
 end
 
-module TimeOfDayIntroduction
+module TimeIntro
   # Player can skip when true
   SKIPPABLE = true
 
@@ -54,14 +58,22 @@ module TimeOfDayIntroduction
   # You can create other introduction periods here
   def self.create_conf_presets
     ret = [
-      Configuration.new(_INTL("Day"), -> (t){ PBDayNight.isDay?(t)}, "Graphics/Pictures/introductionday.png"),
-      Configuration.new(_INTL("Night"), -> (t){ PBDayNight.isNight?(t)}, "Graphics/Pictures/introductionnight.png")
+      Configuration.new(
+        _INTL("Day"),
+        -> (t){ PBDayNight.isDay?(t)},
+        "Graphics/Pictures/Time Intro/day.png"
+      ),
+      Configuration.new(
+        _INTL("Night"),
+        -> (t){ PBDayNight.isNight?(t)},
+        "Graphics/Pictures/Time Intro/night.png"
+      )
     ]
 #    ret = [
-#      Configuration.new(_INTL("Morning"), -> (t){ TimeOfDayIntroduction.isMorningXY?(t)}),
-#      Configuration.new(_INTL("Day"), -> (t){ TimeOfDayIntroduction.isDayXY?(t)}),
-#      Configuration.new(_INTL("Evening"), -> (t){ TimeOfDayIntroduction.isEveningXY?(t)}),
-#      Configuration.new(_INTL("Night"), -> (t){ TimeOfDayIntroduction.isNightXY?(t)})
+#      Configuration.new(_INTL("Morning"), -> (t){ TimeIntro.isMorningXY?(t)}),
+#      Configuration.new(_INTL("Day"), -> (t){ TimeIntro.isDayXY?(t)}),
+#      Configuration.new(_INTL("Evening"), -> (t){ TimeIntro.isEveningXY?(t)}),
+#      Configuration.new(_INTL("Night"), -> (t){ TimeIntro.isNightXY?(t)})
 #    ]
     self.check_consistensy(ret)
     return ret
@@ -84,59 +96,84 @@ module TimeOfDayIntroduction
       for time_conf in presets
         count+=1 if time_conf.proc.call(time)
       end
-      time_string = time.strftime("%I:%M %p %S - %s")
       if count != 1
         raise "For time #{time.strftime("%I:%M %p")} there is #{count} correct configurations. There must be only one."
       end
     end
   end
 
-  def self.temp
-    return $game_temp || $PokemonTemp # Essentials v19- compatibility
-  end
-
   def self.on_map_update
-    return if (temp.in_menu || temp.in_battle || temp.message_window_showing)
-    return if $game_player.move_route_forcing || pbMapInterpreterRunning?
-    return if !GameData::MapMetadata.exists?($game_map.map_id) || !GameData::MapMetadata.get($game_map.map_id).outdoor_map
-    return if temp.last_time_of_day && TimeOfDayIntroduction.get_conf == temp.last_time_of_day
-    temp.last_time_of_day = TimeOfDayIntroduction.get_conf
+    return if !should_show?
+    temp.last_time_of_day = get_conf
     temp.last_time_of_day_yday = pbGetTimeNow.yday
-    return if TimeOfDayIntroduction::SHOW_ONLY_WHEN_CHANGED && temp.last_time_of_day_yday != pbGetTimeNow.yday
+    if SHOW_ONLY_WHEN_CHANGED && temp.last_time_of_day_yday != pbGetTimeNow.yday
+      return
+    end
     frameCount = Graphics.frame_count
     Graphics.update
     has_freeze = frameCount == Graphics.frame_count
     if has_freeze
-      return if !TimeOfDayIntroduction::SHOW_AT_GAME_LOAD
+      return if !SHOW_AT_GAME_LOAD
       Graphics.transition(0)
-      TimeOfDayIntroduction.start_scene
+      start_scene
     else 
-      pbFadeOutIn(99999) { TimeOfDayIntroduction.start_scene }
+      pbFadeOutIn(99999) { start_scene }
     end
+  end
+
+  def self.temp
+    # Essentials v19- compatibility
+    return $game_temp || $PokemonTemp
+  end
+
+  def self.should_show?
+    return (
+      !temp.in_menu && !temp.in_battle && !temp.message_window_showing && 
+      !$game_player.move_route_forcing && !pbMapInterpreterRunning? && (
+        GameData::MapMetadata.exists?($game_map.map_id) && 
+        GameData::MapMetadata.get($game_map.map_id).outdoor_map
+      ) && (!temp.last_time_of_day || get_conf != temp.last_time_of_day)
+    )
   end
  
   class Scene
     def start_scene(time_of_day_conf)
-      @sprites={} 
-      @viewport=Viewport.new(0,0,Graphics.width,Graphics.height)
-      @viewport.z=999999
-      @sprites["background"]=IconSprite.new(0,0,@viewport)
+      @sprites = {} 
+      @viewport = Viewport.new(0,0,Graphics.width,Graphics.height)
+      @viewport.z = 999999
+      @sprites["background"] = IconSprite.new(0,0,@viewport)
       @sprites["background"].bitmap = Bitmap.new(Graphics.width,Graphics.height)
-      @sprites["background"].bitmap.fill_rect(Rect.new(0,0,@sprites["background"].bitmap.width,@sprites["background"].bitmap.height),Color.new(0,0,0))
-      load_image = time_of_day_conf.image_path && !time_of_day_conf.image_path.empty?
-      if load_image
-        @sprites["image"]=IconSprite.new(0,0,@viewport)
+      @sprites["background"].bitmap.fill_rect(Rect.new(
+        0,
+        0,
+        @sprites["background"].bitmap.width,
+        @sprites["background"].bitmap.height
+      ),Color.new(0,0,0))
+      @sprites["image"]=IconSprite.new(0,0,@viewport)
+      if time_of_day_conf.image_path && !time_of_day_conf.image_path.empty?
         @sprites["image"].setBitmap(time_of_day_conf.image_path)
+      end
+      if @sprites["image"].bitmap
         @sprites["image"].x=(Graphics.width-@sprites["image"].bitmap.width)/2
         @sprites["image"].y=(Graphics.height-@sprites["image"].bitmap.height)/2
       end
-      @sprites["messagebox"]=Window_AdvancedTextPokemon.new(time_of_day_conf.name)
+      @sprites["messagebox"] = Window_AdvancedTextPokemon.new(
+        time_of_day_conf.name
+      )
       @sprites["messagebox"].viewport=@viewport
-      if !load_image || !@sprites["image"].bitmap
-        @sprites["messagebox"].x = (Graphics.width-@sprites["messagebox"].width)/2
-        @sprites["messagebox"].y = (Graphics.height-@sprites["messagebox"].height)/2
+      if bitmap_is_empty?(@sprites["image"].bitmap)
+        @sprites["messagebox"].x = (
+          Graphics.width-@sprites["messagebox"].width
+        )/2
+        @sprites["messagebox"].y = (
+          Graphics.height-@sprites["messagebox"].height
+        )/2
       end
       pbFadeInAndShow(@sprites) { update }
+    end
+
+    def bitmap_is_empty?(bitmap)
+      return !bitmap || bitmap.width==32
     end
    
     def update
@@ -145,11 +182,13 @@ module TimeOfDayIntroduction
   
     def main
       wait_frames = Graphics.frame_rate*3
-      for i in 0...wait_frames
+      wait_frames.times do
         Graphics.update
         Input.update
         update
-        if (Input.trigger?(Input::USE) || Input.trigger?(Input::BACK)) && SKIPPABLE
+        if (
+          Input.trigger?(Input::USE) || Input.trigger?(Input::BACK)
+        ) && SKIPPABLE
           break
         end   
       end 
@@ -208,11 +247,11 @@ end
 # Essentials v19- compatibility
 if defined?(Events) && Events.respond_to?(:onMapUpdate) 
   Events.onMapUpdate += proc { |_sender,_e|
-    TimeOfDayIntroduction.on_map_update
+    TimeIntro.on_map_update
   }
 else
   EventHandlers.add(:on_frame_update, :time_of_day_introduction, proc {
-    TimeOfDayIntroduction.on_map_update
+    TimeIntro.on_map_update
   })
 end
 
